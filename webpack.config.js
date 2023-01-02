de3f59bbcc89e766ef85c6ee1d6378e54dc81454
Denis <deonis.20901@gmail.com>
2023-01-02 18:56:39 +0600

    change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry: path.resolve(__dirname, "src"),
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public", "dist"),
         filename: "bundle.js",
     },
     module: {
